757707ca544db84c571fcf36f128d0ebafa98fca
zwen <18514430012@163.com>
2018-05-14 16:54:17 +0800

    出发地目的地车型添加字母搜索

--- Application/Customer/Controller/OrderBookController.class.php
+++ Application/Customer/Controller/OrderBookController.class.php
@@ -60,13 +60,57 @@ class OrderBookController extends BaseController {
 	  */
 	 function mobileBooking(){
 	 	$ret = session("userdata");
-	 	$pro = get_province();
+	 	$proc = get_province();
+         $pro = array();
+        foreach($proc as &$v){
+            $szm = $this->getFirstCharter($v['area_name']);
+            $pro[$szm][] = $v;
+        }
+        ksort($pro);
 	 	$brand = get_brand();
 	 	$this->assign("member",$ret);
 	 	$this->assign("pro",$pro);
 	 	$this->assign("brand",$brand);
 	 	$this->display("OrderInfo:order_one");
 	 }
+
+	 /**
+      * 获取文字首字母
+      */
+    function getFirstCharter($str){
+        if(empty($str)){return '';}
+        $fchar=ord($str{0});
+        if($fchar>=ord('A')&&$fchar<=ord('z')) return strtoupper($str{0});
+        $s1=iconv('UTF-8','gb2312',$str);
+        $s2=iconv('gb2312','UTF-8',$s1);
+        $s=$s2==$str?$s1:$str;
+        $asc=ord($s{0})*256+ord($s{1})-65536;
+        if($asc>=-20319&&$asc<=-20284) return 'A';
+        if($asc>=-20283&&$asc<=-19776) return 'B';
+        if($asc>=-19775&&$asc<=-19219) return 'C';
+        if($asc>=-19218&&$asc<=-18711) return 'D';
+        if($asc>=-18710&&$asc<=-18527) return 'E';
+        if($asc>=-18526&&$asc<=-18240) return 'F';
+        if($asc>=-18239&&$asc<=-17923) return 'G';
+        if($asc>=-17922&&$asc<=-17418) return 'H';
+        if($asc>=-17417&&$asc<=-16475) return 'J';
+        if($asc>=-16474&&$asc<=-16213) return 'K';
+        if($asc>=-16212&&$asc<=-15641) return 'L';
+        if($asc>=-15640&&$asc<=-15166) return 'M';
+        if($asc>=-15165&&$asc<=-14923) return 'N';
+        if($asc>=-14922&&$asc<=-14915) return 'O';
+        if($asc>=-14914&&$asc<=-14631) return 'P';
+        if($asc>=-14630&&$asc<=-14150) return 'Q';
+        if($asc>=-14149&&$asc<=-14091) return 'R';
+        if($asc>=-14090&&$asc<=-13319) return 'S';
+        if($asc>=-13318&&$asc<=-12839) return 'T';
+        if($asc>=-12838&&$asc<=-12557) return 'W';
+        if($asc>=-12556&&$asc<=-11848) return 'X';
+        if($asc>=-11847&&$asc<=-11056) return 'Y';
+        if($asc>=-11055&&$asc<=-10247) return 'Z';
+        return null;
+    }
+
 	 /**
 	  * 下单第一步，判断&&价格
 	  */
@@ -102,7 +146,15 @@ class OrderBookController extends BaseController {
 	        $code = md5($userInfo['user_code']);
 	        S($code,$data);
 	        //dump($data);die();
-            $brand = get_brand();
+            $brandc = get_brand();
+            foreach($brandc as &$v){
+                $szm = $this->getFirstCharter($v['cxjj_brand']);
+                if($v['cxjj_brand'] == '讴歌'){
+                    $szm = 'O';
+                }
+                $brand[$szm][] = $v;
+            }
+            ksort($brand);
             $this->assign("brand",$brand);
 	        $this->assign("data",$data);
 	        $this->assign("name1",explode("-",$data['chufa_name'])[1]);
